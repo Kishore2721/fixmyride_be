c31b65ca9eefac8ceb587af97ef49fd426bbcdfa
kishore2721 <kishoreg2721@gmail.com>
2025-06-11 15:22:57 +0530

    initial backend commit

---
+++ src/main/java/com/project/fixmyride/FixmyrideBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.fixmyride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixmyrideBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixmyrideBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fixmyride/FixmyrideBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fixmyride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixmyrideBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
